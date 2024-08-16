7881b050f5b7b11b78f2423c1ef4f52a5ed23d64
mrjeanp <47196328+mrjeanp@users.noreply.github.com>
2024-08-16 12:14:45 -0700

    removed import

--- bot/handlers/readyListener.ts
+++ bot/handlers/readyListener.ts
@@ -1,6 +1,5 @@
 import { Events } from "discord.js";
 import createListener from "../lib/utils/createListener";
-import { startServer } from "../extensions/lofi/wss";
 
 export default createListener(Events.ClientReady, async (_bot, client) => {
   console.log(`${client.user?.tag} is ready!`);
